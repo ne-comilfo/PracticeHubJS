ac1d1b259c27a0792eb7863e740497b5abb16b51
Grigorii Malyshev <grisha.malyshev.06@bk.ru>
2025-08-25 15:29:51 +0300

    added functional add film in films list

--- js/pages/library/library.js
+++ js/pages/library/library.js
@@ -1,3 +1,64 @@
 import loadHeader from "../../services/load-header";
+import addForm from "./modules/add-form";
 
 loadHeader();
+
+let index = +localStorage.getItem('index') || 0;
+
+const form = document.getElementById('add-form'),
+    filmsField = document.querySelector('.library-films');
+
+uploadFilms();
+
+function changeFilmProgress(id, newProgress) {
+    const films = JSON.parse(localStorage.getItem('films'));
+    films.forEach(film => {
+        if (film.index == id) {
+            film.progress = newProgress;
+        }
+    })
+    localStorage.setItem('films', JSON.stringify(films));
+}
+
+function saveFilms({ title, mark, year, genre, progress, index }) {
+    let films = JSON.parse(localStorage.getItem('films')) || [];
+    films.push({
+        title: title,
+        mark: mark,
+        year: year,
+        genre: genre,
+        progress: progress,
+        index: index
+    });
+    localStorage.setItem('films', JSON.stringify(films));
+}
+
+function uploadFilms() {
+    let films = JSON.parse(localStorage.getItem('films')) || [];
+    films.forEach(film => {
+        addForm(film, filmsField);
+    })
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
+    index++;
+    localStorage.setItem('index', index);
+    data['index'] = index;
+    saveFilms(data);
+    addForm(data, filmsField,);
+
+    form.reset();
+})
+
+document.addEventListener('change', (e) => {
+    const target = e.target;
+    if (target.classList.contains('film-status')) {
+        const card = target.closest('.film-card');
+        const id = card.getAttribute('data-id');
+        const newStatus = target.value
+        changeFilmProgress(id, newStatus);
+    }
+})
---
+++ js/pages/library/modules/add-form.js
@@ -0,0 +1,36 @@
+
+
+function addForm({ title, mark, year, genre, progress, index }, filmsField) {
+    const progressData = ['В планах', 'Смотрю', 'Просмотрено'];
+    const progressInput = [];
+    progressData.forEach(item => {
+        if (progress !== item) {
+            progressInput.push(`<option value="${item}">${item}</option>`);
+        } else {
+            progressInput.push(`<option value="${item}" selected>${item}</option>`);
+        }
+    })
+    const filmCard = `
+        <li class="film-card" data-id='${index}'>
+                    <h3 class="film-title">${checkLength(capitalize(title))}</h3>
+                    <p class="film-year">${year}</p>
+                    <p class="film-rating">${mark}</p>
+                    <select class="film-status">
+                        ${progressInput.join('\n')}
+                    </select>
+                    <p class="film-genre">${genre}</p>
+                </li>
+    `;
+
+    filmsField.insertAdjacentHTML('beforeend', filmCard);
+}
+
+function checkLength(string) {
+    return string.length >= 11 ? string.slice(0, 10) + '...' : string;
+}
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+export default addForm;
